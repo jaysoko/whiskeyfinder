d5af9b8c7ebef963b6669dec3d5637b7a3109447
Jay Sokolowski <jaysoko@gmail.com>
2019-04-09 02:36:10 +0000

    Updates

--- lib/whiskeyfinder.rb
+++ lib/whiskeyfinder.rb
@@ -2,5 +2,9 @@ require_relative "./whiskeyfinder/version"
 require_relative "./whiskeyfinder/cli"
 require_relative "./whiskeyfinder/whiskey"
 
+require 'nokogiri'
+require 'pry'
+require 'open-uri'
+
 
 
--- lib/whiskeyfinder/whiskey.rb
+++ lib/whiskeyfinder/whiskey.rb
@@ -2,22 +2,24 @@ class WhiskeyFinder::Whiskey
 attr_accessor :name, :price, :distiller, :country
 
 
+
 def self.find_whiskeys
+  #find whiskeys from a certain country
+  whiskeys = []
   
-#find whiskeys from a certain country
+  whiskeys << self.scrape_whiskeys
 
 end
 
 
+def self.scrape_whiskeys
+  #Go to MasterofMalt
+  doc = Nokogiri::HTML(open("https://www.masterofmalt.com/country/american-whiskey/"))
+  binding.pry 
   
-  
-  
-  
-  
-  
-  
-  
-  
+end
+
+
   
   
   
