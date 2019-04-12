7420e8715ea01f3acbbbf5cbb7ba6a1ca9a8d993
Jay Sokolowski <jaysoko@gmail.com>
2019-04-12 21:30:59 +0000

    Updates with Scraping

--- lib/whiskeyfinder/whiskey.rb
+++ lib/whiskeyfinder/whiskey.rb
@@ -15,10 +15,10 @@ end
 def self.scrape_whiskeys
   #Go to MasterofMalt
   doc = Nokogiri::HTML(open("https://www.masterofmalt.com/country/american-whiskey/"))
-  name = doc.search("h3 a").first.text
-  distiller = doc.css("#ContentPlaceHolder1_ctl00_AnchorDistillery").text
-  price = doc.css("#ContentPlaceHolder1_ctl00_pricesWrapper").text
-  alcohol_content = doc.css("#ContentPlaceHolder1_ctl00_VolumeAbv").text
+  name = doc.search("h3 a").text
+  distiller = doc.css("a.product-box-wide-distillery.gold").text
+  price = doc.css("div.product-box-wide-price.gold").text
+  alcohol_content = doc.css("div.product-box-wide-volume.gold").text
   binding.pry
 end
 
