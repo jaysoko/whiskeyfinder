7af3779510ebe59e79461844815412d43a09a824
Jay Sokolowski <jaysoko@gmail.com>
2019-04-14 02:16:21 +0000

    Updates

--- lib/whiskeyfinder/cli.rb
+++ lib/whiskeyfinder/cli.rb
@@ -16,22 +16,37 @@ class WhiskeyFinder::CLI
   def menu
     puts "Enter The Number Of The Country You Wish To Research:"
     input = gets.strip
+    counter = 0
     case input
       when "1"
-        puts "More Info on American Whiskeys"
-        @american_url = "https://www.masterofmalt.com/country/american-whiskey/"
+        puts "More Information on American Whiskeys"
         @index_url = @american_url
         @american_list = WhiskeyFinder::Whiskey.find_whiskeys(@index_url, @country="USA")
-        #@american_list.each {|w| puts "#{w[:name]}" }
-        @american_list.flatten.map { |w| puts "#{w[:name]}" }
+        @american_list.flatten.map{ |w| puts "#{counter+=1}.\t#{w[:name]}\n\tDistiller: #{w[:distiller]}" }
+        puts "Enter The Number Of The Whiskey For More Info:"
+        input = gets.strip
       when "2"
         puts "More Info on Japanese Whiskeys"
+        @index_url = @japanese_url
+        @japanese_list = WhiskeyFinder::Whiskey.find_whiskeys(@index_url, @country="Japan")
+        @japanese_list.flatten.map{ |w| puts "#{counter+=1}.\t#{w[:name]}\n\tDistiller: #{w[:distiller]}" }
+        puts "Enter The Number Of The Whiskey For More Info:"
+        input = gets.strip
       when "3"
         puts "More Info on Irish Whiskeys"
+        @index_url = @irish_url
+        @irish_list = WhiskeyFinder::Whiskey.find_whiskeys(@index_url, @country="Ireland")
+        @irish_list.flatten.map{ |w| puts "#{counter+=1}.\t#{w[:name]}\n\tDistiller: #{w[:distiller]}" }
+        more_details_menu
       when "exit"
          goodbye
       end
   end
+
+def more_details_menu
+  puts "Enter The Number Of The Whiskey For More Info:"
+  input = gets.strip
+end
 
   def goodbye
     puts "Thank You For Playing"
